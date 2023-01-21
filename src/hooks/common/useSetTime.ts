fcb3408175fc559dd89cb5c00b8d6316e4a245ea
leejy001 <greatness007@naver.com>
2023-01-21 17:23:15 +0900

    fix: time 조건 수정

--- src/hooks/common/useSetTime.ts
+++ src/hooks/common/useSetTime.ts
@@ -42,14 +42,14 @@ const useSetTime = (): [
   }, []);
 
   const setImage = (hour: number) => {
-    if (hour >= 0 || hour < 6)
+    if (hour >= 0 && hour < 6)
       return `${process.env.PUBLIC_URL}/asset/night.png`;
-    if (hour >= 6 || hour < 12)
+    if (hour >= 6 && hour < 12)
       return `${process.env.PUBLIC_URL}/asset/morning.png`;
-    if (hour >= 12 || hour < 18)
+    if (hour >= 12 && hour < 18)
       return `${process.env.PUBLIC_URL}/asset/noon.png`;
     if (hour >= 18 && hour < 24)
-      return `${process.env.PUBLIC_URL}/asset/night.png`;
+      return `${process.env.PUBLIC_URL}/asset/evening.png`;
   };
 
   const year = date.getFullYear();
